3a04f8ccd61eb6be80c7cf010ed7f49410c458d6
John Moustakas <john.moustakas@gmail.com>
2024-03-30 10:48:42 -0700

    add gather scripts and main-subset sample

--- py/rrevaluator/io.py
+++ py/rrevaluator/io.py
@@ -5,7 +5,7 @@ rrevaluator.io
 I/O tools.
 
 """
-import os
+import os, pdb
 import fitsio
 import numpy as np
 from glob import glob
@@ -13,6 +13,7 @@ from astropy.table import Table, vstack
 
 from desiutil.log import get_logger
 log = get_logger()
+
 
 def read_vi(quality=2.5, vi_spectype=None, samplefile=None, veto_fibers=True, main=False):
     """See https://data.desi.lbl.gov/doc/releases/edr/vac/vi/
@@ -83,3 +84,60 @@ def read_vi(quality=2.5, vi_spectype=None, samplefile=None, veto_fibers=True, ma
     return allvi
 
 
+def read_iron_main_subset(samplefile=None, veto_fibers=True):
+    """Read the subset of Iron/main tiles identified by Jaime.
+
+    https://desisurvey.slack.com/archives/C05QG3VBBPB/p1709747811273839
+
+    I've created a sample of TILEIDs starting with tiles-iron.fits and then
+    applying some cuts. These cuts divide the tiles into three ranges (low,
+    middle, high) based on percentiles for three parameters: 'EXPTIME',
+    'EFFTIME_SPEC', and 'TILEDEC'. These ranges are applied to both bright and
+    dark programs, resulting in 27 permutations for each program and a total of
+    54 tiles.  The file I'm sharing contains two columns: TILEID and
+    NTILE_SIMILAR. Here, NTILE_SIMILAR indicates the count of tiles falling
+    within a similar range determined by the specified criteria. For example,
+    this could represent the number of tiles with high 'EXPTIME', low
+    'EFFTIME_SPEC', and high 'TILEDEC'.
+
+    """
+    def _build_sample():
+        specprod = 'iron'
+    
+        tilefile = '/global/cfs/cdirs/desicollab/users/ioannis/Y3-templates/sample/tiles-iron-main-subset.csv'
+        tilelist = Table.read(tilefile)
+        log.info(f'Read {len(tilelist):,d} tiles from {tilefile}')
+    
+        reduxdir = os.path.join(os.getenv('DESI_ROOT'), 'spectro', 'redux', specprod, 'tiles', 'cumulative')
+    
+        sample = []
+        for tileid in tilelist['TILEID']:
+            for petal in range(10):
+                coaddfile = glob(os.path.join(reduxdir, str(tileid), '*', f'coadd-{petal}-{tileid}-thru*.fits'))
+                if len(coaddfile) > 0:
+                    coaddfile = coaddfile[0]
+                    objtypes = fitsio.read(coaddfile, ext='FIBERMAP', columns='OBJTYPE')
+                    rows = np.where(objtypes == 'TGT')[0]
+                    fm = Table(fitsio.read(coaddfile, ext='FIBERMAP', columns=['TARGETID', 'TILEID', 'PETAL_LOC', 'FIBER', 'TARGET_RA', 'TARGET_DEC'], rows=rows))
+                    log.info(f'Read {len(fm):,d} objects from {coaddfile}')            
+                    sample.append(fm)
+        sample = vstack(sample)
+        return sample
+    
+    if samplefile is not None and os.path.isfile(samplefile):
+        sample = Table(fitsio.read(samplefile))
+        log.info(f'Read {len(sample):,d} objects from {samplefile}')
+    else:
+        sample = _build_sample()
+        if samplefile is not None:
+            log.info(f'Writing {len(sample):,d} objects to {samplefile}')        
+            sample.write(samplefile, overwrite=True)
+
+    # veto bad fibers
+    if veto_fibers:
+        veto = np.loadtxt('/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/unique_badfibers.txt')
+        I = np.isin(sample['FIBER'], veto)
+        log.info(f'Vetoing {np.sum(I):,d}/{len(sample):,d} targets on bad fibers.')
+        sample = sample[~I]
+
+    return sample
